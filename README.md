323f64cd12186a5632d25a54f232db6f8a2ab03d
dipikabaad <dipika.baad@gmail.com>
2018-07-18 07:35:42 +0200

    Inital code

---
+++ README.md
@@ -0,0 +1,16 @@
+# Working with Vue.js Forms
+
+This repository contains the code for the forms section from the [Vue.js: From Beginner to Professional course](https://l.codingexplained.com/course/vuejs?src=github).
+
+## Getting up and Running
+
+``` bash
+# Install the dependencies
+npm install
+
+# Serve with hot reload at http://localhost:8080
+npm run dev
+
+# Build for production with minification
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>Add Blog Post</h1>
+
+                <form>
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input type="text" class="form-control" id="title" v-model="post.title">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="content">Content</label>
+                        <textarea class="form-control" id="content" v-model="post.content" cols="30" rows="10"></textarea>
+                    </div>
+                </form>
+
+                <hr>
+
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <td class="col-xs-6"><strong>Field</strong></td>
+                            <td class="col-xs-6"><strong>Value</strong></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Title</td>
+                            <td>{{ post.title }}</td>
+                        </tr>
+                        <tr>
+                            <td>Content</td>
+                            <td style="white-space: pre;">{{ post.content }}</td>
+                        </tr>
+                        <tr>
+                            <td>Publish immediately</td>
+                            <td>{{ post.publishImmediately }}</td>
+                        </tr>
+                        <tr>
+                            <td>Share on</td>
+                            <td>
+                                <ul>
+                                    <li v-for="media in post.shareOn">{{ media }}</li>
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Category</td>
+                            <td>{{ post.category }}</td>
+                        </tr>
+                        <tr>
+                            <td>Series</td>
+                            <td>{{ post.series }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                post: {
+                    title: '',
+                    content: '',
+                    publishImmediately: false,
+                    shareOn: [],
+                    category: '',
+                    series: ''
+                },
+                formData: {
+                    socialMedia: ['Facebook', 'Twitter'],
+                    categories: ['Frontend', 'Backend'],
+                    series: [
+                        'Vue.js: From Beginner to Professional',
+                        'Complete Guide to Elasticsearch'
+                    ]
+                }
+            };
+        }
+    }
+</script>
+
+<style>
+    input[type="radio"] + label,
+    input[type="checkbox"] + label {
+        font-weight: normal;
+    }
+</style>
